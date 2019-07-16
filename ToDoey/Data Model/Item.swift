bd2128d373eacb0387147d947b7cf630921fc49a
Andrew Reitenbach <andrew.g.reitenbach@gmail.com>
2019-07-16 16:19:11 -0400

    Convert to MVC design pattern

---
+++ ToDoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  ToDoey
+//
+//  Created by Andrew Grogan Reitenbach on 7/11/19.
+//  Copyright © 2019 Advantagion. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
---
+++ TodoModel.swift
@@ -0,0 +1,17 @@
+//
+//  TodoModel.swift
+//  ToDoey
+//
+//  Created by Andrew Grogan Reitenbach on 7/11/19.
+//  Copyright © 2019 Advantagion. All rights reserved.
+//
+
+import Foundation
+
+class Model {
+    
+    var title : String = ""
+    
+    let done : Bool
+    
+}

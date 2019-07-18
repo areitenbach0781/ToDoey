f33cd33ca7aa3b5c3b1fcd26c98238bf48a2301c
Andrew Reitenbach <andrew.g.reitenbach@gmail.com>
2019-07-18 13:10:53 -0400

    Persisting data using Codable protocol and Plist.

--- ToDoey/Controllers/TodoListViewController.swift
+++ ToDoey/Controllers/TodoListViewController.swift
@@ -11,23 +11,15 @@ import UIKit
 class TodoListViewController: UITableViewController {
 
     var itemArray = [Item]()
-    
-    let defaults = UserDefaults.standard
 
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        print(dataFilePath)
         
-        let newItem = Item()
-        newItem.title = "Find Mike"
-        itemArray.append(newItem)
-        
-        let newItem2 = Item()
-        newItem2.title = "Buy eggs"
-        itemArray.append(newItem2)
-        
-        let newItem3 = Item()
-        newItem3.title = "Destroy Demorgon"
-        itemArray.append(newItem3)
+        loadItems()
         
         
 //        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
@@ -63,8 +55,8 @@ class TodoListViewController: UITableViewController {
         // print(itemArray[indexPath.row])
         
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
-
-        tableView.reloadData()
+        
+        saveItems()
 
         tableView.deselectRow(at: indexPath, animated: true)
         
@@ -83,11 +75,10 @@ class TodoListViewController: UITableViewController {
 
             let newItem = Item()
             newItem.title = (textField.text!)
+            
             self.itemArray.append(newItem)
             
-            self.defaults.set(self.itemArray, forKey: "TodoListArray")
-            
-            self.tableView.reloadData()
+            self.saveItems()
 
         }
         
@@ -103,6 +94,34 @@ class TodoListViewController: UITableViewController {
         
     }
     
+    //MARK -- Model Manipulation Methods
+    
+    func saveItems() {
+    
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Error in coding item array, \(error)")
+        }
+        
+        self.tableView.reloadData()
+        
+        }
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+            itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding item array, \(error)")
+                }
+            }
+        }
+        
     
 }
 
